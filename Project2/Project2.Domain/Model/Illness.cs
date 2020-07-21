da4810449a3bd4f89ffec2ce99a3f227e1dc73ed
Jordan Gamble <jgamble482@gmail.com>
2020-07-20 22:56:52 -0400

    Added Validation for Name Properties of Illness and Nurse along with corresponding Unit Tests

--- Project2/Project2.Domain/Model/Illness.cs
+++ Project2/Project2.Domain/Model/Illness.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace Project2.Domain.Model
 {
     public class Illness
     {
+        private string _name;
         public int IllnessId { get; set; }
-        public string Name { get; set; }
+        public string Name {
+            get
+            {
+                return _name;
+            } 
+
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentNullException("Illness name must have a value", nameof(value));
+                }
+                else if (value.GetType() != typeof(string))
+                {
+                    throw new ArgumentException("Illness name must be a string", nameof(value));
+                }
+                _name = value;
+            }
+        }
 
         public Illness(int id, string name)
         {
             IllnessId = id;
-            Name = name;
+            _name = name;
 
         }
 
---
+++ Project2/Project2.Test/IllnessDomainTests.cs
@@ -0,0 +1,36 @@
+﻿using Project2.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xunit;
+
+namespace Project2.Test
+{
+    public class IllnessDomainTests
+    {
+        private readonly Illness illness = new Illness();
+
+        [Fact]
+
+        public void NameShouldBeSettable()
+        {
+            string testName = "COVID-19";
+            illness.Name = testName;
+
+            Assert.Equal(testName, illness.Name);
+
+        }
+
+        [Fact]
+        public void NameShouldNotAllowNull()
+        {
+
+            Assert.ThrowsAny<ArgumentNullException>(() => illness.Name = string.Empty);
+        }
+
+
+
+
+    }
+}
