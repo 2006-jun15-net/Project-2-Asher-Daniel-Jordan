419e9f00ba554550a0af35ddb94b8cb7b4335d02
Jordan Gamble <jgamble482@gmail.com>
2020-07-25 12:32:39 -0400

    Nurse Controller Complete

--- Project2/Project2.Data/Repository/NurseRepository.cs
+++ Project2/Project2.Data/Repository/NurseRepository.cs
@@ -19,11 +19,67 @@ namespace Project2.Data.Repository
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
+        public async Task<Nurse> CreateNurseAsync(Nurse nurse)
+        {
+            var nurseEntity = new NurseEntity
+            {
+                NurseId = nurse.NurseId,
+                FirstName = nurse.FirstName,
+                LastName = nurse.LastName,
+            };
+
+            _context.NurseEntity.Add(nurseEntity);
+
+            await _context.SaveChangesAsync();
+
+            return nurse;
+
+        }
+
+        public async Task<Nurse> DeleteNurseAsync(Nurse nurse)
+        {
+            var Entity = new NurseEntity
+            {
+                NurseId = nurse.NurseId,
+                FirstName = nurse.FirstName,
+                LastName = nurse.LastName
+            };
+
+            _context.NurseEntity.Remove(Entity);
+
+            await _context.SaveChangesAsync();
+
+            return nurse;
+
+        }
+
+        public async Task<Nurse> GetByNurseIdAsync(int nurseId)
+        {
+            var nurseEntity = await _context.NurseEntity.FindAsync(nurseId);
+            return new Nurse(nurseEntity.NurseId, nurseEntity.FirstName, nurseEntity.LastName);
+        }
+
         public async Task<IEnumerable<Nurse>> GetNursesAsync()
         {
             var Entities = await _context.NurseEntity.ToListAsync();
 
             return Entities.Select(e => new Nurse(e.NurseId, e.FirstName, e.LastName));
+        }
+
+        public async Task<Nurse> UpdateNurseAsync(Nurse nurse)
+        {
+            var Entity = new NurseEntity
+            {
+                NurseId = nurse.NurseId,
+                FirstName = nurse.FirstName,
+                LastName = nurse.LastName
+            };
+
+            _context.Entry(Entity).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return nurse;
         }
     }
 }
